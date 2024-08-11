70cf9314ca10b5edb9eab32eb8496e5d86cebec9
Jesper Hesselgren <jeppehesselgren@gmail.com>
2024-08-11 15:42:14 +0200

    Added className

--- src/components/BlueSquare.jsx
+++ src/components/BlueSquare.jsx
@@ -7,7 +7,7 @@ export default function BlueSquare() {
       <h3>Your result</h3>
       <BlueCircle />
       <h2>Great</h2>
-      <p>Your score higher than 65% of the people who have taken these tests.</p>
+      <p className="score-tex">Your score higher than 65% of the people who have taken these tests.</p>
     </div>
   );
 }
